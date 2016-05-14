deffae768b7f0ea864ba6a681999453671d8ac95
Victor Balan <victor.balan93@gmail.com>
2016-05-14 16:56:20 +0300

    refactor everytyhing

--- app/elements/game/model/gameobject.js
+++ app/elements/game/model/gameobject.js
@@ -1,32 +1,50 @@
-class GameObject{
+class GameObject {
   constructor(x, y, color, dimension) {
     this.object = this._square(x, y, color, dimension)
+    this.dimension = dimension;
   }
 
-  addToStage(stage){
+  addToStage(stage) {
     stage.addChild(this.object);
   }
 
-  getBounds(){
-    return this.object.getBounds();
+  getBounds() {
+    return {
+      x: this.object.x,
+      y: this.object.y,
+      width: this.dimension,
+      height: this.dimension
+    };
   }
 
-  setBounds(bounds){
+  setBounds(bounds) {
     this.object.setBounds(bounds.x, bounds.y, bounds.width, bounds.height);
   }
 
   _square(x, y, color, dimension) {
     var square = new createjs.Shape();
-    square.graphics.beginFill(color).drawRect(x, y, dimension, dimension);
-    square.setBounds(x, y, dimension, dimension);
+    square.graphics.beginFill(color).drawRect(0, 0, dimension, dimension);
+    square.x = x;
+    square.y = y;
+    // square.setBounds(x, y, dimension, dimension);
     return square;
   }
 
-  collidesWith(rect1) {
+  moveTo(x, y) {
+    var b = this.getBounds();
+    this.object.x = x;
+    this.object.y = y;
+  }
+
+  intersects(rect1) {
     var rect2 = this.getBounds();
     return !(rect1.x >= rect2.x + rect2.width
     || rect1.x + rect1.width <= rect2.x
     || rect1.y >= rect2.y + rect2.height
     || rect1.y + rect1.height <= rect2.y);
+  }
+
+  getIntersectionType(rect1) {
+    return this.intersects(rect1) ? 'collision' : 'none';
   }
 }
--- app/elements/game/model/player.js
+++ app/elements/game/model/player.js
@@ -1,10 +1,10 @@
 class Player extends GameObject{
-  constructor(startPos) {
-    super(startPos.x, startPos.y, 'DeepSkyBlue', startPos.height/2)
+  constructor(x, y, dimension) {
+    super(x, y, 'DeepSkyBlue', dimension)
+    this.setBounds({x:x, y:y, width:dimension, height: dimension});
   }
-  moveTo(x, y){
-    this.object.setBounds(x, y, this.getBounds().width, this.getBounds().height);
-    this.object.x = x;
-    this.object.y = y;
+
+  getBounds(){
+    return this.object.getBounds()
   }
 }
