a7b484813a64992176cd7be6b20ed91893a7d88c
Victor Balan <victor.balan93@gmail.com>
2016-05-17 18:08:03 +0300

    add some instructions to the first page

--- app/elements/game/menu/menu.js
+++ app/elements/game/menu/menu.js
@@ -3,7 +3,7 @@ class Menu extends BaseMenu {
     super(stage, width, height, "Fluffy the cat");
     var self = this;
 
-    var txtPlay = this._text("Play", "bold 40px Arial", width / 2, 200, {
+    var txtPlay = this._text("Play", "bold 40px Arial", width / 2, height/4, {
       color: '#FFAA00',
       textAlign: 'center',
       cursor: 'pointer'
@@ -19,6 +19,24 @@ class Menu extends BaseMenu {
     });
     txtPlay.addEventListener('click', onPlay);
     stage.addChild(txtPlay);
+
+    var txtInstructions = this._text("g - God mode(see all map)", "bold 20px Arial", width / 2, height/2, {
+      color: '#FFAA00',
+      textAlign: 'center'
+    })
+    stage.addChild(txtInstructions);
+
+    txtInstructions = this._text("bar legend - the brighter the color is the closer you are to finish", "bold 20px Arial", width / 2, height/2 + 30, {
+      color: '#FFAA00',
+      textAlign: 'center'
+    })
+    stage.addChild(txtInstructions);
+
+    txtInstructions = this._text("backspace - go to the previous menu", "bold 20px Arial", width / 2, height/2 + 60, {
+      color: '#FFAA00',
+      textAlign: 'center'
+    })
+    stage.addChild(txtInstructions);
     stage.update()
   }
 }
